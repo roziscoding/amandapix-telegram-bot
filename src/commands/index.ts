b39fc457471ceaa899eae28dbc28a14a8f6c5acb
Rogério Munhoz <roz@rjmunhoz.me>
2021-05-21 09:41:39 -0300

    fix(commands): fix setinfo naming problem

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,7 +2,7 @@ import cancel from './cancel'
 import clearinfo from './clearinfo'
 import getInfo from './getInfo'
 import help from './help'
-import setInfo from './setInfo'
+import setinfo from './setinfo'
 import start from './start'
 
 export const specialCommands = { cancel }
@@ -10,7 +10,7 @@ export const specialCommands = { cancel }
 export default {
   start,
   clearinfo,
-  setInfo,
+  setinfo,
   getInfo,
   help
 }
