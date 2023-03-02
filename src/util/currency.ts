86f787d7a772de11d51f0df249648f0549282198
Roz <roz@rjmunhoz.me>
2023-03-02 12:18:51 -0300

    feat: create functions to parse multiple currencies

--- src/util/currency.ts
+++ src/util/currency.ts
@@ -1,5 +1,32 @@
-const ensureNumber = (value: string | number) =>
-  typeof value === "string" ? Number(value) : value;
+import { z } from "https://deno.land/x/zod@v3.20.5/mod.ts";
+
+export class CurrencyNotFoundError extends Error {
+  public readonly currency: string;
+
+  constructor(originalMessage: string) {
+    super();
+
+    const currencyRegex = /moeda nao encontrada (?<currency>.*)/i;
+    this.currency = originalMessage.match(currencyRegex)?.groups?.currency ??
+      originalMessage;
+  }
+}
+
+const currencySchema = z.object({
+  code: z.string(),
+  codein: z.string(),
+  name: z.string(),
+  high: z.string().transform(Number),
+  low: z.string(),
+  varBid: z.string(),
+  pctChange: z.string(),
+  bid: z.string(),
+  ask: z.string(),
+  timestamp: z.string(),
+  create_date: z.string(),
+});
+
+const ensureNumber = (value: string | number) => typeof value === "string" ? Number(value) : value;
 
 export const BRL = (amount: number | string) =>
   ensureNumber(amount).toLocaleString("pt-BR", {
@@ -7,3 +34,39 @@ export const BRL = (amount: number | string) =>
     style: "currency",
     minimumFractionDigits: 2,
   });
+
+export function getConversionRates<Rates extends readonly string[]>(
+  currencies: Rates,
+) {
+  const BASE_URL = "https://economia.awesomeapi.com.br/last/";
+
+  const currencyList = currencies
+    .map((currency) => currency.toUpperCase())
+    .filter((currency) => currency !== "BRL")
+    .map((currency) => `${currency}-BRL`)
+    .join(",");
+
+  const url = new URL(currencyList, BASE_URL);
+
+  return fetch(url)
+    .then(async (response) => {
+      const json = await response.json();
+      if (!response.ok && json?.code === "CoinNotExists") {
+        throw new CurrencyNotFoundError(json.message);
+      }
+
+      return json;
+    })
+    .then((currencyData) => {
+      const rates = Object.entries(currencyData).map(([currencyName, currencyRate]) => [
+        currencyName.replace(/BRL/ig, ""),
+        currencySchema.parse(currencyRate).high,
+      ]);
+
+      return Object.fromEntries(rates);
+    }) as { [k in Rates[number]]: number };
+}
+
+export type CurrencyConverstionRates = Awaited<
+  ReturnType<typeof getConversionRates>
+>;
--- src/util/query.ts
+++ src/util/query.ts
@@ -1,4 +1,5 @@
 import { evaluate, round } from "https://esm.sh/mathjs@11.5.0";
+import { CurrencyConverstionRates, getConversionRates } from "./currency.ts";
 
 export function evaluateQuery(query: string): Promise<number | null> {
   try {
@@ -6,4 +7,71 @@ export function evaluateQuery(query: string): Promise<number | null> {
   } catch {
     return Promise.resolve(null);
   }
+}
+
+export async function evaluateConversionQuery(
+  query: string,
+) {
+  try {
+    const values = extractCurrencies(query);
+    const rates = await getConversionRates(
+      values.map(({ currency }) => currency),
+    );
+    const convertedValues = convertValues(values, rates);
+
+    const convertedQuery = buildConvertedQuery(query, convertedValues);
+
+    return {
+      ok: true,
+      finalValue: round(evaluate(convertedQuery), 2),
+      rates,
+      values: convertedValues,
+      finalQuery: convertedQuery,
+    } as const;
+  } catch (error) {
+    return {
+      ok: false,
+      error,
+    } as const;
+  }
+}
+
+export function extractCurrencies(query: string) {
+  const currencyRegex = /(?<value>[0-9,.]+) (?<currency>[a-z]{3})/gi;
+
+  return Array.from(query.matchAll(currencyRegex))
+    .map((match) => ({ ...match.groups }))
+    .filter(Boolean)
+    .filter(({ value, currency }) => value && currency)
+    .map(({ value, currency }) => ({
+      value: Number(value),
+      currency: currency,
+    }));
+}
+
+export function convertValues(
+  values: Array<{ value: number; currency: string }>,
+  rates: CurrencyConverstionRates,
+) {
+  return values.map(({ value, currency }) => ({
+    currency,
+    value,
+    converted: (rates[currency] ?? 1) * value,
+  }));
+}
+
+export function buildConvertedQuery(
+  originalQuery: string,
+  convertedValues: Array<
+    { currency: string; value: number; converted: number }
+  >,
+) {
+  return convertedValues.reduce(
+    (query, conversion) =>
+      query.replace(
+        `${conversion.value} ${conversion.currency}`,
+        conversion.converted.toString(),
+      ),
+    originalQuery.replace(/,/g, "."),
+  );
 }
