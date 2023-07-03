c39c03d114ed051923cb77de4c856af445083bb9
Rogério Munhoz <rogerio.munhoz@pipefy.com>
2023-07-03 13:39:17 -0300

    hotfix: revert grammY version

--- src/deps.ts
+++ src/deps.ts
@@ -1,7 +1,7 @@
 export * from "https://esm.sh/common-tags@1.8.2";
 export { evaluate, round } from "https://esm.sh/mathjs@11.6.0";
 export * from "https://lib.deno.dev/std@0.192/encoding/base64.ts";
-export * from "https://lib.deno.dev/x/grammy@v1/mod.ts";
+export * from "https://lib.deno.dev/x/grammy@v1.16/mod.ts";
 export { conversations, createConversation } from "https://lib.deno.dev/x/grammy_conversations@v1/mod.ts";
 export type { Conversation, ConversationFlavor } from "https://lib.deno.dev/x/grammy_conversations@v1/mod.ts";
 export * from "https://lib.deno.dev/x/grammy_storages@v2/file/src/mod.ts";
@@ -10,3 +10,4 @@ export * from "https://lib.deno.dev/x/pix@v2/mod.ts";
 export { json, serve } from "https://lib.deno.dev/x/sift@0.6/mod.ts";
 export * from "https://lib.deno.dev/x/zod@v3/mod.ts";
 export * from "https://raw.githubusercontent.com/denorg/qrcode/87101e061a7b1f9f9d5ddb304ca8c9e99262e9e1/mod.ts";
+
