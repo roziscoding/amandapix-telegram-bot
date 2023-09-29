5e98df15ed811153ffabb7d55daf4231568f1d77
Rogerio Munhoz <roz@rjmunhoz.me>
2023-09-29 09:27:23 -0300

    chore: add githook and update dependencies

--- src/deps.ts
+++ src/deps.ts
@@ -1,15 +1,14 @@
-export * from "https://deno.land/std@0.198.0/encoding/base64.ts";
+export * from "common-tags";
+export * from "https://deno.land/std@0.203.0/encoding/base64.ts";
 export { Collection, MongoClient } from "https://deno.land/x/atlas_sdk@v1.1.1/mod.ts";
 export { debug } from "https://deno.land/x/debug@0.2.0/mod.ts";
-export * from "https://deno.land/x/grammy@v1.18.1/mod.ts";
+export * from "https://deno.land/x/grammy@v1.19.0/mod.ts";
 export { conversations, createConversation } from "https://deno.land/x/grammy_conversations@v1.1.2/mod.ts";
 export type { Conversation, ConversationFlavor } from "https://deno.land/x/grammy_conversations@v1.1.2/mod.ts";
-export * from "https://deno.land/x/grammy_storages@v2.3.0/file/src/mod.ts";
+export * from "https://deno.land/x/grammy_storages@v2.3.2/file/src/mod.ts";
 export * from "https://deno.land/x/pix@v2.0.6/mod.ts";
 export { json, serve } from "https://deno.land/x/sift@0.6.0/mod.ts";
 export * from "https://deno.land/x/zod@v3.22.2/mod.ts";
 export { z } from "https://deno.land/x/zod@v3.22.2/mod.ts";
-export * from "https://esm.sh/common-tags@2.0.0-alpha.1/";
-export { evaluate, round } from "https://esm.sh/mathjs@11.9.1/";
+export { evaluate, round } from "https://esm.sh/mathjs@11.11.1/";
 export * from "https://raw.githubusercontent.com/denorg/qrcode/87101e061a7b1f9f9d5ddb304ca8c9e99262e9e1/mod.ts";
-
