d6ee47d369d41b8a410a9c47477250ceef0c61d9
Roz <roz@rjmunhoz.me>
2023-11-28 22:25:14 -0300

    chore: fix linter errors

--- src/util/qr-code-url.ts
+++ src/util/qr-code-url.ts
@@ -5,7 +5,7 @@ export type QRCodeUrlFlavor = {
   getQrCodeUrl: (pixCode: string) => string;
 };
 
-export const qrCodeUrl: MiddlewareFn<Context & QRCodeUrlFlavor> = async (ctx, next) => {
+export const qrCodeUrl: MiddlewareFn<Context & QRCodeUrlFlavor> = (ctx, next) => {
   ctx.getQrCodeUrl = (pixCode) => `https://t.me/${ctx.me.username}/qrcode?startapp=${encodeBase64(pixCode)}`;
   return next();
 };
