eaa57a0a083d3b692993f292a637d8560011e7b6
Roz <3948961+roziscoding@users.noreply.github.com>
2024-09-22 13:05:34 -0300

    feat: move to Deno KV (#5)

--- src/config.ts
+++ src/config.ts
@@ -4,10 +4,6 @@ import { z } from "./deps.ts";
 const AppConfig = z.object({
   TELEGRAM_TOKEN: z.string(),
   WEBHOOK_URL: z.string().default(""),
-  DATABASE_ENDPOINT: z.string().default(""),
-  DATABASE_API_KEY: z.string().default(""),
-  DATABASE_DATASOURCE: z.string().default(""),
-  DATABASE_DBNAME: z.string().default(""),
 }).transform((obj) => ({
   ...obj,
   WEBHOOK_SECRET: obj.TELEGRAM_TOKEN.replace(/[^a-zA-Z0-9]/gi, ""),
--- src/middleware/session.ts
+++ src/middleware/session.ts
@@ -1,30 +1,7 @@
-import { AppContext, AppSession, Environment } from "../bot.ts";
-import { AppConfig } from "../config.ts";
-import { FileAdapter, MongoClient, session } from "../deps.ts";
-import { ISession, MongoDBAdapter } from "../util/storage-adapter.ts";
+import { AppContext, AppSession } from "../bot.ts";
+import { DenoKVAdapter, session } from "../deps.ts";
 
-function getStorage(config: AppConfig, environment = Environment.Development) {
-  if (environment === Environment.Development) {
-    return new FileAdapter<AppSession>({ dirName: "sessions" });
-  }
-
-  const client = new MongoClient({
-    dataSource: config.DATABASE_DATASOURCE,
-    endpoint: config.DATABASE_ENDPOINT,
-    auth: {
-      apiKey: config.DATABASE_API_KEY,
-    },
-  });
-
-  const db = client.database(config.DATABASE_DBNAME);
-  const sessions = db.collection<ISession>("sessions");
-
-  return new MongoDBAdapter<AppSession>({
-    collection: sessions,
-  });
-}
-
-export const sessionMiddleware = (config: AppConfig, environment: Environment) =>
+export const sessionMiddleware = (kv: Deno.Kv) =>
   session<AppSession, AppContext>({
     getSessionKey: (ctx) => ctx.from?.id.toString(),
     initial: () => ({
@@ -33,5 +10,5 @@ export const sessionMiddleware = (config: AppConfig, environment: Environment) =
       name: "",
       onboarded: false,
     }),
-    storage: getStorage(config, environment),
+    storage: new DenoKVAdapter<AppSession>(kv),
   });
