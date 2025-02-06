e1479c104e5b248ba7d5aefd59dff2cbbd79b1ce
Roz <roz@rjmunhoz.me>
2025-02-05 21:56:58 -0300

    chore: improve setWebhook endpoint

--- src/endpoints/setWebhook.ts
+++ src/endpoints/setWebhook.ts
@@ -1,13 +1,19 @@
 import { config } from "../config.ts";
 import { Api } from "../deps.ts";
 
-export async function setWebhook() {
+export async function setWebhook(req: Request) {
   const api = new Api(config.TELEGRAM_TOKEN);
   const webhookUrl = new URL(config.WEBHOOK_URL);
   webhookUrl.pathname = "/bot";
 
+  const token = new URL(req.url).searchParams.get("token");
+  if (token !== config.TELEGRAM_TOKEN) {
+    return new Response(undefined, { status: 401 });
+  }
+
   await api.setWebhook(webhookUrl.toString(), {
     secret_token: config.WEBHOOK_SECRET,
+    drop_pending_updates: true,
   });
 
   return new Response(undefined, { status: 204 });
